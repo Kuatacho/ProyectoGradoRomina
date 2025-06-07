15f13d6b93b3732708eac9432e76e67e2819f396
Victor Chavez <chavco8157@gmail.com>
2025-06-07 00:58:53 -0400

    Funcion camara aniadida

--- frontend/src/app/components/learning/learning.ts
+++ frontend/src/app/components/learning/learning.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { Navbar } from "../../shared/navbar/navbar";
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-learning',
-  imports: [Navbar],
+  imports: [Navbar,CommonModule],
   templateUrl: './learning.html',
   styleUrl: './learning.css'
 })
-export class Learning {
+export class Learning implements AfterViewInit {
+  @ViewChild('video') videoElement!: ElementRef<HTMLVideoElement>;
+  cameraError: string | null = null;
 
+  ngAfterViewInit() {
+    this.initCamera();
+  }
+
+  initCamera() {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then((stream) => {
+        this.videoElement.nativeElement.srcObject = stream;
+      })
+      .catch((err) => {
+        this.cameraError = 'No se pudo acceder a la cámara: ' + err.message;
+        console.error(err);
+      });
+  }
 }
